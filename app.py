709ea89dee77a70c03451fcf3d37a2b0093a6d6f
VasudevaReddy Nemali <deva.teche@gmail.com>
2023-11-02 02:30:34 -0500

    Pushing first phase of functionality

--- app.py
+++ app.py
@@ -2,10 +2,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 available_slots = list(range(1, 11))
 selected_slot = None
-
 
 @app.route('/parking', methods=['GET', 'POST'])
 def get_parking_availability():
@@ -24,7 +22,6 @@ def get_parking_availability():
         selected_slot = None
     
     return render_template('index.html', available_slots=available_slots, selected_slot=selected_slot)
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5005, host='0.0.0.0')
